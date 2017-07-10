c7f9fffae2d9162e7735bfdc68426d706de898df
yefan <394818428@qq.com>
2017-07-11 01:10:31 +0800

    修改配置

--- frame-web/src/main/java/com/frame/swagger/SwaggerConfig.java
+++ frame-web/src/main/java/com/frame/swagger/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.frame.swagger;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -18,13 +19,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Value("${host.prefix}")
+
     @Bean
     public Docket buildDocket(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(buildApiInf())
                 .select().apis(RequestHandlerSelectors.basePackage("com.frame.web.controller"))//controller路径
                 .paths(PathSelectors.any())
-                .build();
+                .build().host("");
     }
 
     private ApiInfo buildApiInf(){
